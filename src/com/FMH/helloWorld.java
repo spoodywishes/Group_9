96f603b4d34c75383b182bd19f293ccd6d5a692d
Fmharmanci <71557439+Fmharmanci@users.noreply.github.com>
2021-02-04 21:30:56 +0300

    helloWorld

---
+++ src/com/FMH/helloWorld.java
@@ -0,0 +1,9 @@
+package com.FMH;
+
+public class helloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello group");
+        System.out.println("Hello group again");
+    }
+
+}
