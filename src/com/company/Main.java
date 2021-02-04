efd6eedfee6be2cddea4f57ca2c4d238ad7822fa
Fmharmanci <71557439+Fmharmanci@users.noreply.github.com>
2021-02-04 11:25:08 +0300

    just example

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("welcome all group-9 members");
         System.out.println("Thanks a lot");
+        System.out.println("\"example\" = " + "example");
     }
 }
