b37da7e91f94e59fff0b74ebe98ff9a294cb4219
Fmharmanci <71557439+Fmharmanci@users.noreply.github.com>
2021-02-05 09:52:38 +0300

    Hello World

---
+++ src/com/Fatih/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.Fatih;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println( "HelloWorld");
+        System.out.println( "HelloWorld");
+    }
+}
